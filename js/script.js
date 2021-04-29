f2937c2b5038e38aa0b24c83468db89f01a43f3e
Dmitrii Zimakov <vdimkav@gmail.com>
2021-04-29 18:39:05 +0300

    016

--- js/script.js
+++ js/script.js
@@ -1,52 +1,39 @@
-/* Задание на урок:
-
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
 'use strict';
 
-// Код возьмите из предыдущего домашнего задания
+let num = 20;
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+function showFirstMessage(text) {
+  console.log(text);
+  let num = 10;
+  console.log(num);
+}
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genders: [],
-  privat: false
+showFirstMessage("Hello world");
+console.log(num);
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function calc(a, b) {
+//   return (a + b);
+// }
+
+function ret() {
+  let num = 50;
+  return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+  console.log("Hello");
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt("Один из последних просмотренных фильмов?", ""),
-        b = +prompt("На сколько оцените его?", "");
-  
-  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-    console.log('done');
-  } else {
-    console.log('error');
-    i--;
-  }
-}
+logger();
 
-if (personalMovieDB.count < 10) {
-  console.log('<10');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log('10-30');
-} else if (personalMovieDB.count >= 30) {
-  console.log('>30');
-} else {
-  console.log('error');
-}
-
-console.log(personalMovieDB);
+const calc = (a, b) => {
+  console.log("1");
+  return a + b;
+};
